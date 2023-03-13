383baae50fbc1554f8bcbeee6c8d1b42fb01e094
c-bata <contact@c-bata.link>
2023-03-13 21:14:10 +0900

    Check PR 4449

--- src/init.py
+++ src/init.py
@@ -1,18 +1,7 @@
 import optuna
-import pymssql
 
 from objective import objective_factory
 from optuna.samplers import RandomSampler
-
-
-def create_mssql_database() -> None:
-    conn = pymssql.connect("mssql", "sa", "optuna-test-5ZYB")
-    conn.autocommit(True)
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE optuna")
-
-
-create_mssql_database()
 
 
 print("mysql")
@@ -24,17 +13,5 @@ study2.optimize(objective_factory(allow_inf=True, allow_nan=False), n_trials=10)
 print("sqlite")
 study3 = optuna.create_study(study_name="test", storage="sqlite:///data/sample.db", sampler=RandomSampler())
 study3.optimize(objective_factory(allow_inf=True, allow_nan=False), n_trials=10)
-print("mssql")
-study4 = optuna.create_study(study_name="test", storage="mssql+pymssql://sa:optuna-test-5ZYB@mssql/optuna?charset=utf8", sampler=RandomSampler())
-study4.optimize(objective_factory(allow_inf=False, allow_nan=False), n_trials=10)
-print("redis")
-study5 = optuna.create_study(study_name="test", storage="redis://redis:6379", sampler=RandomSampler())
-study5.optimize(objective_factory(allow_inf=False, allow_nan=False), n_trials=10)
-
-print(study1)
-print(study2)
-print(study3)
-print(study4)
-print(study5)
 
 print("done")
